5a2fae07cf705a13966e7dfe38aeb1ab6c8099cd
atismall <atismall128@gmail.com>
2025-11-24 13:32:08 +0100

    Just revising my javascripts basics

--- Basics/app.js
+++ Basics/app.js
@@ -1,3 +1,9 @@
 //console.lo())
-let name = "John";
-console.log(name);
+let firstName = "David is a good man";
+let secondName = "Smith";
+// let fullName = firstName.concat(" ", secondName);
+// console.log(fullName);
+// firstName += " Something else ";
+// console.log(firstName.toUpperCase());
+// console.log(firstName.slice(0, 2));
+console.log(firstName.split(" "));
