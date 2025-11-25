c302ce0a3d95de3f745fec6431122a148539c0a7
atismall <atismall128@gmail.com>
2025-11-25 13:42:13 +0100

    array method

---
+++ Array/app.js
@@ -0,0 +1,4 @@
+const fruits = ["apples", "pomegranate", "mango"];
+const moreFruits = ["strawberries", "pineapple", "grapefruit"];
+const totalFruits = fruits.concat(moreFruits);
+console.log(totalFruits);
