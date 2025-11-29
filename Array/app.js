143bb4e84b293d43fcc83c55b483239aac231cbe
atismall <atismall128@gmail.com>
2025-11-29 15:40:08 +0100

    commit

--- Array/app.js
+++ Array/app.js
@@ -1,11 +1,2 @@
-const person = {
-  name: "John Doe",
-  age: 20,
-  email: "js@gmail.com",
-  isSubscribed: true,
-  hobbies: ["Reading", "Running", "Cooking"],
-  address: {
-    city: "New York",
-    idk: true,
-  },
-};
+
+  
