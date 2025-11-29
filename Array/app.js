edbd6d1d12edc2e388b7171bfa22b08151cec47b
atismall <atismall128@gmail.com>
2025-11-29 16:56:43 +0100

    c

--- Array/app.js
+++ Array/app.js
@@ -1,2 +1,19 @@
+const date = new Date();
+console.log(date);
 
-  
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+const seconds = date.getSeconds();
+const milliSeconds = date.getMilliseconds();
+
+console.log(`YEAR : ${year}
+MONTH: ${month}
+DAY: ${day}
+HOURS: ${hours}
+MINUTES: ${minutes}
+SECONDS: ${seconds}
+MILLISECONDS: ${milliSeconds}
+  `);
