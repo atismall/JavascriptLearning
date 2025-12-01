0cd8eb5af52682c125a719966fbad570e76d840b
atismall <atismall128@gmail.com>
2025-12-01 18:36:48 +0100

    rest and spread operators

--- Array/app.js
+++ Array/app.js
@@ -1,37 +1,5 @@
-// function user(name, age, work) {
-//   return { name, age, work, intro: () => console.log(`My name is ${name}`) };
-// }
+function user(...userData) {
+  return userData.reduce((total, n) => total + n, 5);
+}
 
-// const huxn = user(20, "david", "programmer");
-// const atibig = user("atibi", 20, "designer");
-
-// console.log(huxn.intro());
-
-// const a = 1,
-//   b = 2,
-//   c = 3;
-
-// const obj = { a, b, c };
-
-// console.log(obj);
-
-// const lib = {
-//   sum: (a, b) => a + b,
-//   mult: (a, b) => a * b,
-// };
-
-// console.log(lib.sum(2, 4), lib.mult(2, 4));
-
-// function getPersionsES6 (name, age, height) {
-//     return {name, age, height }
-// } ;
-// function getPersionsES6(name, age, height) {
-//   console.log(`Good ${name}`);
-//   return { name, age, height };
-// }
-
-// // const getPersionsES6 = (name, age, height) => ({ name, age, height });
-
-// const idea = getPersionsES6("HuSn", 19, 200);
-
-// console.log(idea);
+console.log(user(5, 10, 15));
