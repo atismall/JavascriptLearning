f7d492933e3e3978c4f953208e91c3af6234c172
atismall <atismall128@gmail.com>
2025-12-01 14:49:01 +0100

    revising date and time function

--- Array/app.js
+++ Array/app.js
@@ -1,19 +1,31 @@
 const date = new Date();
 console.log(date);
 
-const year = date.getFullYear();
-const month = date.getMonth();
-const day = date.getDay();
-const hours = date.getHours();
-const minutes = date.getMinutes();
-const seconds = date.getSeconds();
-const milliSeconds = date.getMilliseconds();
+// const year = date.getFullYear();
+// const month = date.getMonth();
+// const day = date.getDay();
+// const hours = date.getHours();
+// const minutes = date.getMinutes();
+// const seconds = date.getSeconds();
+// const milliSeconds = date.getMilliseconds();
 
-console.log(`YEAR : ${year}
-MONTH: ${month}
-DAY: ${day}
-HOURS: ${hours}
-MINUTES: ${minutes}
-SECONDS: ${seconds}
-MILLISECONDS: ${milliSeconds}
-  `);
+// console.log(`YEAR : ${year}
+// MONTH: ${month}
+// DAY: ${day}
+// HOURS: ${hours}
+// MINUTES: ${minutes}
+// SECONDS: ${seconds}
+// MILLISECONDS: ${milliSeconds}
+//   `);
+
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleString());
+// date.setDate(date.getDate() + 1);
+// console.log(date.toDateString());
+
+// setInterval(
+//   () => console.log(`This function will be executed every 2 seconds`),
+//   2000
+// );
+clearInterval();
