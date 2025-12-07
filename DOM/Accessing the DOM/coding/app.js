6a9e220c983c5256e82000fbd2b351d8eb3137fb
atismall <atismall128@gmail.com>
2025-12-07 11:07:43 +0100

    DOM

---
+++ DOM/Accessing the DOM/coding/app.js
@@ -0,0 +1,9 @@
+// getElementsByTagName
+const head = document.getElementsByTagName("h1");
+const headlength = document.getElementsByTagName("h1").length;
+console.log(head);
+console.log(headlength);
+
+console.log(document.getElementById("id-1").innerText);
+// console.log(document.getElementsByClassName("cls"));
+console.log(document.querySelectorAll("li"));
